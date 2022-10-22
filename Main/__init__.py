17b3ef7e1fe10733f9e21a02f7de6375f8032fba
Elfilagas <nikita.n.kapustin@gmail.com>
2022-10-22 13:23:42 +0300

    added json

--- Main/__init__.py
+++ Main/__init__.py
@@ -4,11 +4,19 @@ import csv
 import json
 from datetime import datetime
 
+
 def collect_data():
-    pass
+    cur_date = datetime.now().strftime('%d_%m_%Y')
+
+    response = requests.get(url='https://www.lifetime.plus/api/analysis2')
+
+    with open(f'info_{cur_date}.json', 'w') as file:
+        json.dump(response.json(), file, indent=4, ensure_ascii=False)
+
 
 def main():
     collect_data()
+
 
 if __name__ == '__main__':
     main()
