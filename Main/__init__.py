bcdb58ba1ba4cac0c982332e60c365b8a66f976c
Elfilagas <nikita.n.kapustin@gmail.com>
2022-10-22 13:13:06 +0300

    init

---
+++ Main/__init__.py
@@ -0,0 +1,14 @@
+import requests
+import os
+import csv
+import json
+from datetime import datetime
+
+def collect_data():
+    pass
+
+def main():
+    collect_data()
+
+if __name__ == '__main__':
+    main()
