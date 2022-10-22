b9429a4890552c893492e87528efef55fbef1fa5
Elfilagas <nikita.n.kapustin@gmail.com>
2022-10-22 13:52:51 +0300

    added output

--- Main/__init__.py
+++ Main/__init__.py
@@ -10,8 +10,39 @@ def collect_data():
 
     response = requests.get(url='https://www.lifetime.plus/api/analysis2')
 
-    with open(f'info_{cur_date}.json', 'w') as file:
-        json.dump(response.json(), file, indent=4, ensure_ascii=False)
+    # with open(f'info_{cur_date}.json', 'w') as file:
+    #     json.dump(response.json(), file, indent=4, ensure_ascii=False)
+
+    categories = response.json()['categories']
+    result = []
+    for c in categories:
+        c_name = c.get('name').strip()
+        c_items = c.get('items')
+
+        for item in c_items:
+            item_name = item.get('name').strip()
+            item_price = item.get('price')
+            #item_desc = item.get('description').strip()
+            item_waittime = item.get('days')
+            item_bio = item.get('biomaterial')
+
+            result.append(
+                [c_name, item_name, item_bio, item_price, item_waittime]
+            )
+
+    with open(f'result_{cur_date}.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                'Категория',
+                'Анализ',
+                'Биоматериал',
+                'Стоимость',
+                'Готовность в днях'
+            )
+        )
+
+        writer.writerows(result)
 
 
 def main():
